c95dd0458913936c7ae3d19da9606d120538e469
Gorka Magaña <gorka@gorkamm.com>
2019-03-23 17:14:54 +0100

    Added ddp and uil params used by AC3256/10 series

--- airctrl/airctrl.py
+++ airctrl/airctrl.py
@@ -169,6 +169,16 @@ class AirClient(object):
         if 'aqil' in status:
             aqil = status['aqil']
             print('[aqil]  Light brightness: {}'.format(aqil))
+        if 'uil' in status:
+            uil = status['uil']
+            uil_str = {'1': 'ON', '0': 'OFF'}
+            uil = uil_str.get(uil, uil)
+            print('[uil]   Buttons light: {}'.format(uil))
+        if 'ddp' in status:
+            ddp = status['ddp']
+            ddp_str = {'1': 'PM2.5', '0': 'IAI'}
+            ddp = ddp_str.get(ddp, ddp)
+            print('[ddp]   Used index: {}'.format(ddp))
         if 'wl' in status:
             wl = status['wl']
             print('[wl]    Water level: {}'.format(wl))
@@ -226,6 +236,8 @@ def main():
     parser.add_argument('--rhset', help='set target humidity', choices=['40','50','60','70'])
     parser.add_argument('--func', help='set function', choices=['P','PH'])
     parser.add_argument('--aqil', help='set light brightness', choices=['0','25','50','75','100'])
+    parser.add_argument('--uil', help='set button lights on/off', choices=['0','1'])
+    parser.add_argument('--ddp', help='set indicator pm2.5/IAI', choices=['0','1'])
     parser.add_argument('--dt', help='set timer', choices=['0','1','2','3','4','5'])
     parser.add_argument('--cl', help='set child lock', choices=['True','False'])
     parser.add_argument('--wifi', help='read wifi options', action='store_true')
@@ -263,6 +275,10 @@ def main():
         values['func'] = args.func
     if args.aqil:
         values['aqil'] = int(args.aqil)
+    if args.ddp:
+        values['ddp'] = args.ddp
+    if args.uil:
+        values['uil'] = args.uil
     if args.dt:
         values['dt'] = int(args.dt)
     if args.cl:
