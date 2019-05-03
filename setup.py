d28bb8b5f5e44f393ce5fd57fcfff192c6cb3fee
Radoslav Gerganov <rgerganov@vmware.com>
2019-05-03 09:40:59 +0300

    bump version to 0.5.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py-air-control",
-    version="0.0.3",
+    version="0.5.0",
     author="Radoslav Gerganov",
     author_email="rgerganov@gmail.com",
     description="Command line program for controlling Philips air purifiers",
