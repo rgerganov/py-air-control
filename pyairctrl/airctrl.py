b37b2d7956f77219aa0190cf814216e98e479ee0
Harald Heigl <hh-developing@heigl-online.at>
2020-05-18 21:04:27 +0000

    small fixes for output, added firmware and filter

--- pyairctrl/airctrl.py
+++ pyairctrl/airctrl.py
@@ -663,21 +663,26 @@ class HTTPAirClientBase(ABC):
         self.port = port
         self.debug = debug
 
+    def _get_info_for_key(self, key, current_value):
+        if key in statusTransformer:
+            info = statusTransformer[key]
+            if not info[1] is None:
+                current_value = info[1](current_value)
+                if current_value is None:
+                    return None
+            return info[0].format(current_value)
+        else:
+            return '{}: {}'.format(key, current_value)
+
     def _dump_status(self, status, debug=False):
         if debug==True:
-            print("Raw status: " + str(status))
+            print("Raw status:")
+            pprint.pprint(status)
         for key in status:
             current_value=status[key]
-            
-            if key in statusTransformer:
-                info = statusTransformer[key]
-                if not info[1] is None:
-                    current_value = info[1](current_value)
-                    if current_value is None:
-                        continue
-                name_and_value = info[0].format(current_value)
-            else:
-                name_and_value = '{}: {}'.format(key, current_value)
+            name_and_value=self._get_info_for_key(key, current_value)
+            if name_and_value is None:
+                continue
 
             print('[{key}]\t{name_and_value}'.format(key=key, name_and_value=name_and_value).expandtabs(30))
 
@@ -701,6 +706,32 @@ class HTTPAirClientBase(ABC):
     @abstractmethod
     def _set(self, key, value):
         pass
+
+    def get_firmware(self):
+        status = self._get()
+        if status is None:
+            print("No version-info found")
+            return
+
+        print(self._get_info_for_key("swversion", status["swversion"] if "swversion" in status else "nA"))
+        print(self._get_info_for_key("ota", status["ota"] if "ota" in status else "nA"))
+
+    def get_filters(self):
+        status = self._get()
+        if status is None:
+            print("No version-info found")
+            return
+
+        if "fltsts0" in status: print(self._get_info_for_key("fltsts0", status["fltsts0"]))
+        if "fltsts1" in status: print(self._get_info_for_key("fltsts1", status["fltsts1"]))
+        if "fltsts2" in status: print(self._get_info_for_key("fltsts2", status["fltsts2"]))
+        if "wicksts" in status: print(self._get_info_for_key("wicksts", status["wicksts"]))
+
+    def get_wifi(self):
+        print("Getting wifi credentials is currently not supported when using CoAP. Use the app instead.")
+
+    def set_wifi(self, ssid, pwd):
+        print("Setting wifi credentials is currently not supported when using CoAP. Use the app instead.")
 
 class Version107Client(HTTPAirClientBase):
     def __init__(self, host, port=5683, debug=False):
