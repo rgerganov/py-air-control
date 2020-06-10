3bcee04f6ac7a5433bfa4971be97c34da75fc13c
Harald Heigl <hh-developing@heigl-online.at>
2020-06-10 22:17:15 +0000

    removed load_key

--- pyairctrl/airctrl.py
+++ pyairctrl/airctrl.py
@@ -117,9 +117,6 @@ class HTTPAirCli:
         wifi = self._client.set_wifi(ssid, pwd)
         pprint.pprint(wifi)
 
-    def load_key(self):
-        self._client.load_key()
-
     def _dump_status(self, status, debug=False):
         if debug:
             pprint.pprint(status)
@@ -387,45 +384,66 @@ class PlainCoAPAirCli:
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--ipaddr', help='IP address of air purifier')
-    parser.add_argument('--protocol', help='set the communication protocol', choices=['http', 'coap', 'plain_coap'], default='http')
-    parser.add_argument('-d', '--debug', help='show debug output', action='store_true')
-    parser.add_argument('--om', help='set fan speed', choices=['1','2','3','s','t'])
-    parser.add_argument('--pwr', help='power on/off', choices=['0','1'])
-    parser.add_argument('--mode', help='set mode', choices=['P','A','S','M','B','N'])
-    parser.add_argument('--rhset', help='set target humidity', choices=['40','50','60','70'])
-    parser.add_argument('--func', help='set function', choices=['P','PH'])
-    parser.add_argument('--aqil', help='set light brightness', choices=['0','25','50','75','100'])
-    parser.add_argument('--uil', help='set button lights on/off', choices=['0','1'])
-    parser.add_argument('--ddp', help='set indicator pm2.5/IAI/Humidity', choices=['0','1','3'])
-    parser.add_argument('--dt', help='set timer', choices=['0','1','2','3','4','5','6','7','8','9','10','11','12'])
-    parser.add_argument('--cl', help='set child lock', choices=['True','False'])
-    parser.add_argument('--wifi', help='read wifi options', action='store_true')
-    parser.add_argument('--wifi-ssid', help='set wifi ssid')
-    parser.add_argument('--wifi-pwd', help='set wifi password')
-    parser.add_argument('--firmware', help='read firmware', action='store_true')
-    parser.add_argument('--filters', help='read filters status', action='store_true')
+    parser.add_argument("--ipaddr", help="IP address of air purifier")
+    parser.add_argument(
+        "--protocol",
+        help="set the communication protocol",
+        choices=["http", "coap", "plain_coap"],
+        default="http",
+    )
+    parser.add_argument("-d", "--debug", help="show debug output", action="store_true")
+    parser.add_argument("--om", help="set fan speed", choices=["1", "2", "3", "s", "t"])
+    parser.add_argument("--pwr", help="power on/off", choices=["0", "1"])
+    parser.add_argument(
+        "--mode", help="set mode", choices=["P", "A", "S", "M", "B", "N"]
+    )
+    parser.add_argument(
+        "--rhset", help="set target humidity", choices=["40", "50", "60", "70"]
+    )
+    parser.add_argument("--func", help="set function", choices=["P", "PH"])
+    parser.add_argument(
+        "--aqil", help="set light brightness", choices=["0", "25", "50", "75", "100"]
+    )
+    parser.add_argument("--uil", help="set button lights on/off", choices=["0", "1"])
+    parser.add_argument(
+        "--ddp", help="set indicator pm2.5/IAI/Humidity", choices=["0", "1", "3"]
+    )
+    parser.add_argument(
+        "--dt",
+        help="set timer",
+        choices=["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"],
+    )
+    parser.add_argument("--cl", help="set child lock", choices=["True", "False"])
+    parser.add_argument("--wifi", help="read wifi options", action="store_true")
+    parser.add_argument("--wifi-ssid", help="set wifi ssid")
+    parser.add_argument("--wifi-pwd", help="set wifi password")
+    parser.add_argument("--firmware", help="read firmware", action="store_true")
+    parser.add_argument("--filters", help="read filters status", action="store_true")
     args = parser.parse_args()
 
     if args.ipaddr:
-        devices = [ {'ip': args.ipaddr} ]
+        devices = [{"ip": args.ipaddr}]
     else:
-        if args.protocol in ['coap', 'plain_coap']:
-            print('Autodetection is not supported when using CoAP. Use --ipaddr to set an IP address.')
+        if args.protocol in ["coap", "plain_coap"]:
+            print(
+                "Autodetection is not supported when using CoAP. Use --ipaddr to set an IP address."
+            )
             sys.exit(1)
 
         devices = HTTPAirCli.ssdp(debug=args.debug)
         if not devices:
-            print('Air purifier not autodetected. Try --ipaddr option to force specific IP address.')
+            print(
+                "Air purifier not autodetected. Try --ipaddr option to force specific IP address."
+            )
             sys.exit(1)
 
     for device in devices:
-        if args.protocol == 'http':
-            c = HTTPAirCli(device['ip'])
-        elif args.protocol == 'plain_coap':
-            c = PlainCoAPAirCli(device['ip'])
-        elif args.protocol == 'coap':
-            c = CoAPCli(device['ip'], debug=args.debug)
+        if args.protocol == "http":
+            c = HTTPAirCli(device["ip"])
+        elif args.protocol == "plain_coap":
+            c = PlainCoAPAirCli(device["ip"])
+        elif args.protocol == "coap":
+            c = CoAPCli(device["ip"], debug=args.debug)
 
         if args.wifi:
             c.get_wifi()
@@ -442,25 +460,25 @@ def main():
 
         values = {}
         if args.om:
-            values['om'] = args.om
+            values["om"] = args.om
         if args.pwr:
-            values['pwr'] = args.pwr
+            values["pwr"] = args.pwr
         if args.mode:
-            values['mode'] = args.mode
+            values["mode"] = args.mode
         if args.rhset:
-            values['rhset'] = int(args.rhset)
+            values["rhset"] = int(args.rhset)
         if args.func:
-            values['func'] = args.func
+            values["func"] = args.func
         if args.aqil:
-            values['aqil'] = int(args.aqil)
+            values["aqil"] = int(args.aqil)
         if args.ddp:
-            values['ddp'] = args.ddp
+            values["ddp"] = args.ddp
         if args.uil:
-            values['uil'] = args.uil
+            values["uil"] = args.uil
         if args.dt:
-            values['dt'] = int(args.dt)
+            values["dt"] = int(args.dt)
         if args.cl:
-            values['cl'] = (args.cl == 'True')
+            values["cl"] = args.cl == "True"
 
         if values:
             c.set_values(values, debug=args.debug)
@@ -468,5 +486,5 @@ def main():
             c.get_status(debug=args.debug)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
