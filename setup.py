57f626e82dfbc888f4795ed2a7efcbb3c1505686
Radoslav Gerganov <rgerganov@gmail.com>
2020-11-24 09:28:26 +0200

    Version 2.2.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="py-air-control",
-    version="2.1.0",
+    version="2.2.0",
     author="Radoslav Gerganov",
     author_email="rgerganov@gmail.com",
     description="Command line program for controlling Philips air purifiers",
